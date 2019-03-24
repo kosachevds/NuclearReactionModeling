5981c81632949c071021d254acfea1e669073cc2
Daniil <kos-dan@mail.ru>
2019-03-24 19:20:59 +0300

    Remade with 'Vector3D'

--- Particle.cs
+++ Particle.cs
@@ -2,34 +2,28 @@ namespace NuclearReactionModeling
 {
     class Particle
     {
-        public Particle(Point coordinates, Velocity velocity)
+        public Particle(Vector3D coordinates, Velocity velocity)
         {
             Coordinates = coordinates;
             Velocity = velocity;
         }
 
-        public Point Coordinates { get; set; }
+        public Vector3D Coordinates { get; set; }
 
         public Velocity Velocity { get; set; }
 
-        public double X => this.Coordinates.Z;
-
-        public double Y => this.Coordinates.Y;
-
-        public double Z => this.Coordinates.Z;
-
         public void Move(double length)
         {
-            this.Coordinates = new Point(
-                this.X + length * this.Velocity.X,
-                this.Y + length * this.Velocity.Y,
-                this.Z + length * this.Velocity.Z
+            this.Coordinates = new Vector3D(
+                this.Coordinates.X + length * this.Velocity.X,
+                this.Coordinates.Y + length * this.Velocity.Y,
+                this.Coordinates.Z + length * this.Velocity.Z
             );
         }
 
         public Particle CopyWithNewVelocity()
         {
-            return new Particle(this.Coordinates, Velocity.Generate())
+            return new Particle(this.Coordinates, Velocity.Generate());
         }
     }
 }
