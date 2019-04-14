ef0fffec182eeb03a81e6313e020f69772db7768
Daniil Kosachev <kos-dan@mail.ru>
2019-04-14 16:12:46 +0300

    Added method WriteValuesInLinesAsync

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace NuclearReactionModeling
 {
@@ -9,6 +11,16 @@ namespace NuclearReactionModeling
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static void DifferentShapes(string filename)
+        {
+            var beginPointsCount = 5;
+            var lambda = 0.5;
+            var volume = 1.0;
+            var side = Math.Pow(volume, 1.0 / 3.0);
+            var radius = side * Math.Pow(3.0 / (4.0 * Math.PI), 1.0 / 3.0);
+
         }
 
         static List<Particle> GenerateParticlesOnSurface(IShape shape, int count)
@@ -23,6 +35,17 @@ namespace NuclearReactionModeling
             var particles = GenerateParticlesOnSurface(shape, particlesCount);
             var reaction = new NuclearReaction(shape, particles, lambda);
             return reaction.Run();
+        }
+
+        static async Task WriteValuesInLinesAsync<TValue>(List<TValue> values, string filename)
+        {
+            using (var writer = new StreamWriter(File.OpenWrite(filename)))
+            {
+                foreach (var value in values)
+                {
+                    await writer.WriteAsync($"{value}\n");
+                }
+            }
         }
     }
 }
