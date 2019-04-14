36814d243422e820f59e9250068713c7a03fdc65
Daniil Kosachev <kos-dan@mail.ru>
2019-04-14 16:02:10 +0300

    Added methods GenerateParticlesOnSurface and DoReaction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NuclearReactionModeling
 {
@@ -7,6 +9,20 @@ namespace NuclearReactionModeling
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static List<Particle> GenerateParticlesOnSurface(IShape shape, int count)
+        {
+            return Enumerable.Range(0, count)
+                .Select(_ => new Particle(shape.RandomPointFromSurface(), Vector3.GetRandom()))
+                .ToList();
+        }
+
+        static List<int> DoReaction(IShape shape, int particlesCount, double lambda)
+        {
+            var particles = GenerateParticlesOnSurface(shape, particlesCount);
+            var reaction = new NuclearReaction(shape, particles, lambda);
+            return reaction.Run();
         }
     }
 }
